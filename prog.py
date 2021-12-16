33fac561a27ffa8bf1d76d822b285bae5edded58
Emmanuel Goudot <Emman.uel.Goudot@gmail.com>
2021-12-16 14:20:07 +0100

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+# Group 1
+
+
+
+
+# Group 2
+
+
+# Group 3
+
+
+
+# Group 4
+
+
+
+
+# Programme principal
+
+df = Tirage2()
+print(df)
+df = Tirage3()
+print(df)
+df = Tirage4()
+print(df)
+df = Tirage5()
+print(df)
