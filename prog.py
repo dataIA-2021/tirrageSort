ecbd441af352790a014b157a65f6cf28f3cd0afb
Eric_Domas <ecdm3725@gmail.com>
2021-12-16 15:57:12 +0100

    Fonction de tirage aléatoire pour les veilles

--- prog.py
+++ prog.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+data = pd.read_csv("/home/maison/ProjetGit/tirrageSort/liste_apprenants.csv")
 
 # Group 1
 
@@ -7,6 +8,15 @@ import pandas as pd
 
 
 # Group 2
+
+def Tirage2(data):
+    data_without_prof = data.loc[data["Prof"]!=1].reset_index(drop=True)
+    df = data_without_prof['Prénom'].sample(n=len(data_without_prof)).reset_index(drop=True)
+    df1ip =df.iloc[::2].reset_index()
+    df1p=df = df.iloc[1::2].reset_index()
+    res = pd.concat([df1p,df1ip],axis =1,ignore_index=True)
+    resultat = res[[1,3]]
+    return(resultat)
 
 
 # Group 3
@@ -20,11 +30,11 @@ import pandas as pd
 
 # Programme principal
 
-df = Tirage2()
-print(df)
-df = Tirage3()
+d = Tirage2(data)
+print(d)
+'''df = Tirage3()
 print(df)
 df = Tirage4()
 print(df)
 df = Tirage5()
-print(df)
+print(df)'''
