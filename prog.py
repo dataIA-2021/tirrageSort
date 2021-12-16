11814cde35856199760d61edefa573e8b3e6f2cb
KyllianBD <kyllianblanchet@gmail.com>
2021-12-16 14:20:46 +0100

    Tirage3()
    Ajout de Tirage3()
    
    Co-Authored-By: Fdelys <71888063+Fdelys@users.noreply.github.com>

--- prog.py
+++ prog.py
@@ -7,15 +7,35 @@ import pandas as pd
 
 
 # Group 2
-fichier = "liste_apprenants.csv"
-df = pd.read_csv(fichier)
+from random import*
 
-dfa = df[df.Prof==0]
-dfa.describe()
-listt= dfa["Prénom"].tolist()
+def Tirage3():
+    #Renvoie un dataframe contenant des groupes de 3 personnes
 
-# for r in range (df):
+    fichier = "liste_apprenants.csv"
+    df = pd.read_csv(fichier)
+    dfa = df[df.Prof==0]
     
+    Grp = 3 
+    Tirage=[]
+    
+    for k in range (Grp):
+        Tirage.append([])
+    
+    L=["Marceline","Steve","Victorien","Jeremy","Eric","Clara","César","Tim","Alexia","Noli","Tess","Célia","François","Louis","Sylvain","Kyllian"]
+    N = 16
+    
+    for k in range(N):
+        r = round(random()*(N-k)-1)
+        if k%3 == 0 :
+            Tirage[0].append(L[r])
+        elif k%3 == 1:
+            Tirage[1].append(L[r])
+        elif k%3 == 2:
+            Tirage[2].append(L[r])
+        L.pop(r)
+    
+    return(pd.DataFrame(Tirage))
 # Group 3
 
 
