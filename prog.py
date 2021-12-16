97a49902ae1abf518dc2bb4ab056a637e37e48bb
Fdelys <frandel932@hotmail.com>
2021-12-16 14:35:55 +0100

    Update prog.py
    Test de push

--- prog.py
+++ prog.py
@@ -7,8 +7,15 @@ import pandas as pd
 
 
 # Group 2
+fichier = "liste_apprenants.csv"
+df = pd.read_csv(fichier)
 
+dfa = df[df.Prof==0]
+dfa.describe()
+listt= dfa["Prénom"].tolist()
 
+# for r in range (df):
+    
 # Group 3
 
 
@@ -20,11 +27,11 @@ import pandas as pd
 
 # Programme principal
 
-df = Tirage2()
-print(df)
-df = Tirage3()
-print(df)
-df = Tirage4()
-print(df)
-df = Tirage5()
-print(df)
+# df = Tirage2()
+# print(df)
+# df = Tirage3()
+# print(df)
+# df = Tirage4()
+# print(df)
+# df = Tirage5()
+# print(df)
